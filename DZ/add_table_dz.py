050e5647ce57518909026be2efd4f09ff99f68ae
Andrey Kropotin <tork713@gmail.com>
2024-01-29 16:12:13 +0700

    сделал контрольнуюточку 14.23 ещё раз

---
+++ DZ/add_table_dz.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+
+def main():
+
+    item_ID = int(input('Введите ID объекта: '))
+    item_Name = input('Введите наименование объекта: ')
+    item_Price = float(input('Введите стоимость объекта: '))
+
+    conn = sqlite3.connect('inventory.db')
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS Inventory (ItemId INTEGER PRIMARY KEY NOT NULL,
+                    ItemName TEXT NOT NULL, Price REAL NOT NULL)''')
+    cur.execute('''INSERT INTO Inventory (ItemId, ItemName, Price)
+                VALUES (?, ?, ?)''',
+                (item_ID, item_Name, item_Price))
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
--- DZ/dz_2.py
+++ DZ/dz_2.py
@@ -2,9 +2,9 @@ import sqlite3
 
 
 def main():
-    conn = sqlite3.connect('Inventory.db')
+    conn = sqlite3.connect('inventory_dz2.db')
     cur = conn.cursor()
-    cur.execute('''INSERT INTO Inventory (ItemID, ItemName, Price)
+    cur.execute('''INSERT INTO inventory_dz2 (ItemID, ItemName, Price)
                 VALUES (10, 'Циркулярная Пила', 199.99)''')
     conn.commit()
     conn.close()
