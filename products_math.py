b71b4c843d4b4ef0ed752fcf8412af803f9facd7
Andrey Kropotin <tork713@gmail.com>
2024-02-01 09:00:07 +0700

    Начал агрегатные функции/SUM

--- products_math.py
+++ products_math.py
@@ -2,10 +2,13 @@ import sqlite3
 
 
 def main():
+    conn = sqlite3.connect('chocolate.db')
+    cur = conn.cursor()
+    cur.execute('SELECT SUM(UnitsOnHand) FROM Products')
+    res = cur.fetchone()[0]
+    print(res)
 
-
-
-
+    conn.close()
 
 
 if __name__ == '__main__':
